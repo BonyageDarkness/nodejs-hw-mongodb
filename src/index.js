05fee4e9c6654f335b9a24b956d9471b72f1735c
Bohdan <bosrok.ke@gmail.com>
2024-11-20 21:18:19 +0000

    Yeap!1!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+import { startServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  startServer();
+};
+
+bootstrap();
