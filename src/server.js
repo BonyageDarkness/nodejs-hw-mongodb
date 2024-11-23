8cedbd5c5ff7970e20bffa200ee39c5c2362abd7
Bohdan <bosrok.ke@gmail.com>
2024-11-23 22:45:02 +0000

    Create post, delete, patch, errors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
-
-import { getContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 
@@ -28,51 +29,12 @@ export const startServer = () => {
       message: 'Hello world!',
     });
   });
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getContacts();
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('/contacts', contactsRouter);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      status: 500,
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use('*', notFoundHandler); // Обработчик маршрутов, которые не найдены
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      res.status(404).json({
-        status: 404,
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      status: 404,
-      message: 'Not found',
-    });
-  });
+  app.use(errorHandler); // Глобальный обработчик ошибок
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
